562fbb7760b9f7532ae06abae063866c8c71c404
sanika <sanika.pavale@fractal.ai>
2025-09-03 10:05:59 +0530

    changes done

--- stramlit_app.py
+++ stramlit_app.py
@@ -3,3 +3,10 @@ from demo import area_circle
 
 st.header("Hello,streamlit!")
 st.write("This is a simple calculation application")        
+
+radius=st.text_input("Enter radious:")
+
+if st.button("Calculate Area"):
+
+    area=area_circle(int(radius))
+    st.write(f"The are of circle with radius {radius} is {area}.")
